14ca3adfe3f7a33a8c4f44798ea7ca18ede34d09
web-mech <webmech@gmail.com>
2024-10-25 17:12:58 -1000

    fix(*): rename file

--- trans3.py
+++ trans3.py
@@ -6,7 +6,7 @@ device = "cpu" #"cuda" if torch.cuda.is_available() else "cpu"
 model = whisper.load_model("base", device=device)
 
 # Transcribe the audio file
-result = model.transcribe("/Users/webmech/dev/dev916/cupcake/foo.mp3")
+result = model.transcribe("foo.mp3")
 
 # Print the transcription text
 print(result["text"])
